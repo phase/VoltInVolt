9995bbb507e8d86e586f6f14d2c8617284d4f3a1
Bernard Helyer <b.helyer@gmail.com>
2014-04-03 02:31:44 +1300

    volt: Name build 'volta'.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -x 
-volt src/*.volt src/volt/*.volt src/volt/token/*.volt src/volt/ir/*.volt src/volt/parser/*.volt src/volt/util/*.volt
+volt -o volta src/*.volt src/volt/*.volt src/volt/token/*.volt src/volt/ir/*.volt src/volt/parser/*.volt src/volt/util/*.volt
