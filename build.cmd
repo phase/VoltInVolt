2ad195a92bfbd4b6997b61371c9d7f8cba76a98d
Bernard Helyer <b.helyer@gmail.com>
2014-04-24 22:50:05 +1200

    visitor: Initial parsing fixes.

--- build.cmd
+++ build.cmd
@@ -1,1 +1,1 @@
-volt %* -o volta src\*.volt src\volt\*.volt src\volt\token\*.volt src\volt\util\*.volt src\volt\ir\*.volt src\volt\parser\*.volt src\volt\semantic\*.volt
+volt %* -o volta src\*.volt src\volt\*.volt src\volt\token\*.volt src\volt\util\*.volt src\volt\ir\*.volt src\volt\parser\*.volt src\volt\semantic\*.volt src\volt\visitor\*.volt
