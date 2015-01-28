5a12765e2144c4c9b1e3472be7ae81efc85bcc4e
Bernard Helyer <b.helyer@gmail.com>
2015-01-28 20:35:19 +1300

    volta: Start trying to build semantic.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -x 
-volt -o volta src/*.volt src/volt/*.volt src/volt/token/*.volt src/volt/ir/*.volt src/volt/parser/*.volt src/volt/util/*.volt src/volt/semantic/*.volt
+volt -o volta src/*.volt src/volt/*.volt src/volt/token/*.volt src/volt/ir/*.volt src/volt/parser/*.volt src/volt/util/*.volt src/volt/semantic/*.volt src/volt/visitor/*.volt
