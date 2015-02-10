5d5eeb8cea1424b7d111b3ffc99536f3e450f005
Bernard Helyer <b.helyer@gmail.com>
2015-02-10 14:18:15 +1300

    volt: Test volta in build.cmd script.

--- build.cmd
+++ build.cmd
@@ -1,1 +1,3 @@
 volt %* -o volta src\*.volt src\volt\*.volt src\volt\token\*.volt src\volt\util\string.volt src\volt\ir\*.volt src\volt\parser\*.volt
+
+volta src\*.volt src\volt\*.volt src\volt\token\*.volt src\volt\util\string.volt src\volt\ir\*.volt src\volt\parser\*.volt
