9ac956f7aadcf267c6bb26c9b17d2f21eab361ed
Bernard Helyer <b.helyer@gmail.com>
2015-02-25 22:07:47 +1300

    volt: Make windows build a little more sophisticated.

--- build.cmd
+++ build.cmd
@@ -1,3 +1,10 @@
-volt %* -o volta src\*.volt src\volt\*.volt src\volt\token\*.volt src\volt\util\string.volt src\volt\ir\*.volt src\volt\parser\*.volt
+@echo off
 
+
+echo VOLT volta.exe
+if exist volta.exe del volta.exe
+volt %* -o volta src\*.volt src\volt\*.volt src\volt\token\*.volt src\volt\util\string.volt src\volt\ir\*.volt src\volt\parser\*.volt
+if not %ERRORLEVEL% equ 0 exit /b
+
+echo VOLTA
 volta src\*.volt src\volt\*.volt src\volt\token\*.volt src\volt\util\string.volt src\volt\ir\*.volt src\volt\parser\*.volt
