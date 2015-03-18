72aa7fd9f810d67b39e53bec38b8cbf1bb71e92b
Bernard Helyer <b.helyer@gmail.com>
2015-03-18 18:19:49 +1300

    volt: Add flag var to build.cmd.

--- build.cmd
+++ build.cmd
@@ -3,8 +3,8 @@
 
 echo VOLT volta.exe
 if exist volta.exe del volta.exe
-volt %* -o volta src\*.volt src\volt\*.volt src\volt\token\*.volt src\volt\util\string.volt src\volt\ir\*.volt src\volt\parser\*.volt
+volt %* -o volta src\*.volt src\volt\*.volt src\volt\token\*.volt src\volt\semantic\*.volt src\volt\util\*.volt src\volt\ir\*.volt src\volt\parser\*.volt src\volt\visitor\*.volt src\volt\llvm\*.volt src\lib\llvm\*.volt src\lib\llvm\c\*.volt %VFLAGS%
 if not %ERRORLEVEL% equ 0 exit /b
 
 echo VOLTA
-volta src\*.volt src\volt\*.volt src\volt\token\*.volt src\volt\util\string.volt src\volt\ir\*.volt src\volt\parser\*.volt
+volta src\*.volt src\volt\*.volt src\volt\token\*.volt src\volt\util\*.volt src\volt\semantic\*.volt src\volt\ir\*.volt src\volt\parser\*.volt src\volt\visitor\*.volt src\volt\llvm\*.volt src\lib\llvm\*.volt src\lib\llvm\c\*.volt
